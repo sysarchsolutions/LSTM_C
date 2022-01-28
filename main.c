3d15c3cb02975bb7bb53c1612305cbd8693abdfe
sysarchsolutions <41337612+sysarchsolutions@users.noreply.github.com>
2022-01-28 22:30:45 +0800

    Update main.c

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ extern int get_col(char *filename);
 extern void get_two_dimension(char *line, double **dataset, char *filename);
 
 void main(){
- char filename[] = "data.csv";
+    char filename[] = "data.csv";
     char line[1024];
     int row = get_row(filename);
     int col = get_col(filename);
@@ -19,14 +19,14 @@ void main(){
     printf("col = %d\n",col);
     double **dataset = (double **)malloc(row*sizeof(int *));
     int i;
- for (i = 0; i < row; ++i){
-  dataset[i] = (double *)malloc(col*sizeof(double));
- }
-  get_two_dimension(line, dataset, filename);
+    for (i = 0; i < row; ++i){
+        dataset[i] = (double *)malloc(col*sizeof(double));
+    }
+    get_two_dimension(line, dataset, filename);
     double l_rate = 0.01;
- int n_epoch = 100;
- int n_folds = 4;
- int fold_size;
+    int n_epoch = 100;
+    int n_folds = 4;
+    int fold_size;
     fold_size=(int)(row/n_folds);
- evaluate_algorithm(dataset, n_folds, fold_size, l_rate, n_epoch,col,row);
+    evaluate_algorithm(dataset, n_folds, fold_size, l_rate, n_epoch,col,row);
 }
